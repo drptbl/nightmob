2aa71664859a0bad7c13ed2ea4b117b230fecf26
Jakub Mucha <jakub.mucha@yougov.com>
2016-02-25 15:42:52 +0100

    small fixes

--- setUp/setupProxy.js
+++ setUp/setupProxy.js
@@ -1,17 +1,36 @@
 var Proxy = require('browsermob-proxy').Proxy;
 
 exports.setupProxy = function(nightwatch, callback) {
-  var nightwatchClient = nightwatch.client;
-      nightwatchClient.proxy = new Proxy({ host: 'localhost', port: 8080 });;
 
+
+  var nightwatchClient = nightwatch.client || nightwatch,
+    _proxyHost = 'localhost';
+  _proxyPort = '9095';
+
+  nightwatchClient.proxy = new Proxy({
+    host: _proxyHost,
+    port: _proxyPort
+  });
+
+  console.log('Requesting new proxy port from: ' + _proxyHost + ':' + _proxyPort);
   nightwatchClient.proxy.start(function(err, data) {
-    var proxyPort = data.port,
-        proxyUrl = 'localhost' + ':' +  proxyPort;
 
-    nightwatchClient.desiredCapabilities.proxy = { httpProxy: proxyUrl };
+    if (err !== null) {
+      // Console.error("Proxy Connection error: " + err);
+      throw new Error('Failed to connect to Proxy: ' + err);
+    }
+    var proxyPort = data.port;
+
+    var proxyUrl = 'localhost' + ':' + proxyPort;
+    nightwatchClient.options.desiredCapabilities.proxy = {
+      proxyType: 'manual',
+      httpProxy: proxyUrl
+    };
+    console.log('Setup proxy [" + JSON.stringify(nightwatchClient.options.desiredCapabilities.proxy.httpProxy) + "] with port [" + proxyPort + "]');
+
     nightwatchClient.proxyPort = proxyPort;
-
-    if (typeof callback === "function") {
+    console.log('proxyPort: ' + nightwatchClient.proxyPort);
+    if (typeof callback === 'function') {
       callback();
     }
   });
